6d0d67cab664386eff5afecbc53306e25e1b464f
Filip Salindrija <filip.sh@protonmail.com>
2022-08-13 16:55:13 +0200

    Added split resize keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -21,5 +21,9 @@ keymap('i', '<Tab>', 'coc#pum#visible() ? coc#pum#next(1) : "\\<Tab>"', { expr =
 keymap('i', '<S-Tab>', 'coc#pum#visible() ? coc#pum#prev(1) : "\\<S-Tab>"', { expr = true })
 keymap('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "\\<CR>"', { expr = true })
 
-
+-- Resize split
+keymap('n', '<Leader>hd', ':resize -4<CR>')
+keymap('n', '<Leader>hi', ':resize +4<CR>')
+keymap('n', '<Leader>vd', ':vertical resize -4<CR>')
+keymap('n', '<Leader>vi', ':vertical resize +4<CR>')
 
